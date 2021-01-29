b87b175ff1e1904bf767a39b903c746fd6e839ff
NoelW95 <53453274+noelw19@users.noreply.github.com>
2021-01-30 12:48:44 +1300

    completed app, so far

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import RecipeCard from './Components/Recipe/Recipe';
+
 function App() {
+
+  const APP_ID = 1;
+
+  const [ recipes, setRecipes ] = useState([]);
+  const [ search, setSearch ] = useState('');
+  const [ query, setQuery ] = useState('Margarita');
+  const [ searchType, setSearchType ] = useState(`https://www.thecocktaildb.com/api/json/v1/${APP_ID}/search.php?s=${query}`);
+
+  const searchByAlc = `https://www.thecocktaildb.com/api/json/v1/${APP_ID}/filter.php?i=${query}`;
+
+  const getCocktails = async() => {
+    const response = await fetch(searchType);
+    const data = await response.json();
+    console.log(data.drinks)
+    setRecipes(data.drinks)
+  }
+
+  useEffect(() => {
+    getCocktails();
+  }, [query])
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  const checkbox = () => {
+    setSearchType(searchByAlc)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Noels Cocktail App</h1>
+      <form onSubmit={getSearch} className='search-form'>
+        <input className='search-bar' type='text' value={search} onChange={updateSearch} />
+        <button className='search-button' type='submit'>Search</button>
+        <input type='checkbox' onChange={checkbox}/>
+        <p>Search by Alcohol</p>
+      </form>
+
+      <div className='recipe'>
+        {recipes.map(recipe => (
+          <RecipeCard
+            key={recipe.idDrink}
+            title={recipe.strDrink}
+            image={recipe.strDrinkThumb}
+            method={recipe.strInstructions}
+            ing1={recipe.strIngredient1}
+            ing2={recipe.strIngredient2}
+            ing3={recipe.strIngredient3}
+            ing4={recipe.strIngredient4}
+            ing5={recipe.strIngredient5}
+            ing6={recipe.strIngredient6}
+            ing7={recipe.strIngredient7}
+            ing8={recipe.strIngredient8}
+            ing9={recipe.strIngredient9}
+            ing10={recipe.strIngredient10}
+            msr1={recipe.strMeasure1}
+            msr2={recipe.strMeasure2}
+            msr3={recipe.strMeasure3}
+            msr4={recipe.strMeasure4}
+            msr5={recipe.strMeasure5}
+            msr6={recipe.strMeasure6}
+             />
+            
+        ))}
+      </div>
+
     </div>
   );
 }
---
+++ src/Components/Recipe/Recipe.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({title, image, method, ing1, ing2, ing3, ing4, ing5, ing6, ing10, msr1, msr2, msr3, msr4, msr5, msr6}) => {
+
+
+    return (
+        <div className={style.recipeContainer}>
+            <h1>{title}</h1>
+            <img className={style.image} src={image} alt=''/>
+            <div className={style.ingredients}>
+                {!{ing1} ? null : <p className={style.firstP}>{msr1} {ing1}</p>}
+                {!{ing2} ? null : <p>{msr2} {ing2}</p>}
+                {!{ing3} ? null : <p>{msr3} {ing3}</p>}
+                {!{ing4} ? null : <p>{msr4} {ing4}</p>}
+                {!{ing5} ? null : <p>{msr5} {ing5}</p>}
+                {!{ing6} ? null : <p>{msr6} {ing6}</p>}
+                {!{ing10} ? null : <p>{ing10}</p>}
+            </div>
+            <p className={style.method}>{method}</p>
+        </div>
+    )
+}
+
+//trying to sort a way for me to pull in all the different ingredients
+// maybe using a new function to loop through numbers 1 - 10 
+// and only rendering the ingredients that have something within them
+/* const getIngredients = () => {
+        for(let i = 0; i < 10; i++) {
+            if(i) {
+
+            }
+        }
+} */
+
+export default Recipe;
